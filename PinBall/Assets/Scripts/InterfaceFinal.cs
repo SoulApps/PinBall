75b2be3b360ed05066a0adafde994b782cce6f8a
Ramón Guardia López <ramon.guardialopez@gmail.com>
2018-02-10 18:30:36 +0100

    v 0.6 added awake and final scripts and scenes
    Added new scenes.

---
+++ PinBall/Assets/Scripts/InterfaceFinal.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InterfaceFinal : MonoBehaviour {
+
+    public Text textThrown;
+    public Text textDestroyed;
+
+	// Use this for initialization
+	void Start () {
+        textThrown.text = string.Format("Final score: {0}", GameManager.points);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Click()
+    {
+        SceneManager.LoadScene("Awake");
+    }
+}
