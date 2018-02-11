ebbeacae2f939602dd7baaccf8831dc417255d4f
Ramón Guardia López <ramon.guardialopez@gmail.com>
2018-02-11 22:16:31 +0100

    v 0.7
    added items

---
+++ PinBall/Assets/Scripts/GameCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+
+    private AudioSource audioSource;
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag.Equals("Storm"))
+        {
+            collision.gameObject.GetComponent<AudioSource>().Play();
+        }
+    }
+}
